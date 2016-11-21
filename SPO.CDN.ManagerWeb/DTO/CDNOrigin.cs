ed1df7aed36db2de49382a7b9f43c619b997b510
vman <vardhaman.rd@gmail.com>
2016-11-21 10:25:21 +0000

    creating a dev branch

--- SPO.CDN.ManagerWeb/DTO/CDNOrigin.cs
+++ SPO.CDN.ManagerWeb/DTO/CDNOrigin.cs
@@ -6,8 +6,11 @@ using System.Web;
 namespace SPO.CDN.ManagerWeb.DTO
 {
     public class CDNOrigin
-    {
+    {   
+        //Url of the Origin
         public string Url { get; set; }
+
+        //Id of the Origin
         public string ID { get; set; }
     }
 }
