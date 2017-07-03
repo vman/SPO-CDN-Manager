e4b56c8c3c5bc6070920ae109a1713229af161f0
vman <vardhaman.rd@gmail.com>
2017-07-03 23:02:20 +0100

    started refactoring origins

--- SPO.CDN.ManagerWeb/Scripts/src/actions/originActions.ts
+++ SPO.CDN.ManagerWeb/Scripts/src/actions/originActions.ts
@@ -1,4 +1,5 @@
 import { ActionTypes, Action } from './actionTypes';
+import * as api from '../api';
 
 //Action Creators to create and return Actions
 export const requestOrigins = (): Action => ({
@@ -14,3 +15,24 @@ export const requestOriginsError = (error: string): Action => ({
 	type: ActionTypes.FETCH_ORIGINS_ERROR,
 	payload: error
 });
+
+const addNewOriginRequest = (): Action => ({
+	type: ActionTypes.ADD_NEW_ORIGIN_REQUEST
+});
+
+export function addNewOrigin() {
+
+	return async (dispatch: any) => {
+
+		dispatch(addNewOriginRequest());
+
+		try {
+			const result: string[] = await api.post('/Home/GetCDNSettings');
+
+			dispatch(requestOriginsSuccess(result));
+
+		} catch (error) {
+			dispatch(requestOriginsError(error));
+		}
+	};
+}
