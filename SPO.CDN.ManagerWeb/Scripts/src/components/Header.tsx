327e3aff6c362aea79bd2df17cc796f4a5128a50
vman <vardhaman.rd@gmail.com>
2017-07-09 23:27:30 +0100

    added contact

--- SPO.CDN.ManagerWeb/Scripts/src/components/Header.tsx
+++ SPO.CDN.ManagerWeb/Scripts/src/components/Header.tsx
@@ -15,6 +15,9 @@ export class Header extends React.Component<IHeaderProps, IHeaderState> {
 			<span className={[FontClassNames.xxLarge, FontSizes.xxLarge].join(' ')}>
 				Manage Office 365 Public CDN Settings for <span className={[FontClassNames.xLarge, FontSizes.xLarge].join(' ')}>{this.props.SPOSiteUrl}</span>
 			</span>
+			<span className='o365Manager-contact'>
+				by <a target='_blank' href='https://twitter.com/vrdmn'>@vrdmn</a>
+			</span>
 		</div>;
 	}
 }
